4dca96a8810a1a0a8749ec2a13e7fae1219abc2a
anmol dhanda <rk352714@gmail.com>
2023-12-21 21:21:56 +0530

    ui ux completed

--- js/index.js
+++ js/index.js
@@ -10,3 +10,15 @@ searchbarinputfield.addEventListener("blur", () => {
   searchbarinputfield.style.display = "none";
   searchsongicon.style.display = "block";
 });
+
+// show hide the current playing songs containr left side panel
+const parentcontainer = document.querySelector(".parentcontainer");
+const currentsongscontainer = document.querySelector(".currentsongscontainer");
+const currentplayingsongiconindicator = document.getElementById(
+  "currentplayingsongiconindicator"
+);
+currentplayingsongiconindicator.addEventListener("click", () => {
+  parentcontainer.classList.toggle("showhideleftdetailsarea");
+  currentsongscontainer.classList.toggle("showhidecurrentsongscontainer");
+  currentplayingsongiconindicator.classList.toggle("green");
+});
